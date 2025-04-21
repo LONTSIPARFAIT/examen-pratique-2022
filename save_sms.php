57f0eecc302f462a5f88a7617c518679933b877d
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-21 09:23:32 +0100

    exam

--- save_sms.php
+++ save_sms.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enregistrer un SMS</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./save_sms.css">
 </head>
 <body>
     <div class="container">
