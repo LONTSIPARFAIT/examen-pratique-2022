55b7b7bfaf2503e9e212e3cb8c95dd2395b8c371
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-21 09:07:02 +0100

    exam

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "nom_de_votre_base";
+$dbname = "examen";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
