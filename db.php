d2eea8d97bc336f17ca71cc7e37723045d64d670
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-21 09:06:50 +0100

    exam

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php 
 
-// Database connection
+// Connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "examen";
+$dbname = "nom_de_votre_base";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
 }
