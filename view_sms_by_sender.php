851769fc968905bce0062630b304a84e78ca3ee4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-22 13:13:30 +0100

    commit1

--- view_sms_by_sender.php
+++ view_sms_by_sender.php
@@ -130,7 +130,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['expediteur'])) {
                 </tbody>
             </table>
         <?php elseif ($_SERVER["REQUEST_METHOD"] == "POST" && empty($sms_list) && empty($error)): ?>
-            <p class="no-data">Aucun SMS trouvé pour c expéditeur.</p>
+            <p class="no-data">Aucun SMS trouvé pour ce expéditeur.</p>
         <?php endif; ?>
     </div>
 </body>
